b311509e83f78a9c804619d92f61612a4d865a54
Abhimanyu P <abhimanyupazhani@gmail.com>
2026-07-26 23:52:46 +0530

    gpio_Init

--- MCU 1/005 stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h
+++ MCU 1/005 stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h
@@ -16,9 +16,9 @@
 
 typedef struct
 {
-	uint8_t GPIO_PinNumber;
-	uint8_t GPIO_PinMode;
-	uint8_t GPIO_PinSpeed;
+	uint8_t GPIO_PinNumber;			/*!< Possible values from  @GPIO_PIN_NUMBER >!*/
+	uint8_t GPIO_PinMode;  			/*!< Possible values from  @GPIO_PIN_MODES >!*/
+	uint8_t GPIO_PinSpeed; 			/*!< Possible values from  @GPIO_PIN_SPEEDS >!*/
 	uint8_t GPIO_PinPuPdControl;
 	uint8_t GPIO_PinOPType;
 	uint8_t GPIO_PinAltFunMode;
@@ -37,6 +37,65 @@ typedef struct
 	GPIO_PinConfig_t GPIO_PinConfig; //This holds the GPIO pin configuration settings
 
 }GPIO_Handle_t;
+
+/*
+ * @GPIO_PIN_NUMBER
+ * GPIO Pin Numbers
+ */
+#define GPIO_PIN_NO_0 		0
+#define GPIO_PIN_NO_1 		1
+#define GPIO_PIN_NO_2 		2
+#define GPIO_PIN_NO_3 		3
+#define GPIO_PIN_NO_4 		4
+#define GPIO_PIN_NO_5 		5
+#define GPIO_PIN_NO_6 		6
+#define GPIO_PIN_NO_7 		7
+#define GPIO_PIN_NO_8 		8
+#define GPIO_PIN_NO_9 		9
+#define GPIO_PIN_NO_10 		10
+#define GPIO_PIN_NO_11 		11
+#define GPIO_PIN_NO_12		12
+#define GPIO_PIN_NO_13 		13
+#define GPIO_PIN_NO_14		14
+#define GPIO_PIN_NO_15 		15
+
+/*
+ * @GPIO_PIN_MODES
+ * GPIO Pin Possible modes
+ */
+#define GPIO_MODE_IN 		0
+#define GPIO_MODE_OUT		1
+#define GPIO_MODE_ALTFN		2
+#define GPIO_MODE_ANALOG 	3
+#define GPIO_MODE_IT_FT		4    //IT = Input; FT = Falling Edge or time.
+#define GPIO_MODE_IT_RT		5
+#define GPIO_MODE_IT_RFT	6    //RFT = Rising/falling Edge
+
+
+/*
+ * GPIO Pin Possible OUTPUT types
+ */
+#define GPIO_OP_TYPE_PP		0 //PP = Push Pull
+#define GPIO_OP_TYPE_OD		1 //OD = Open Drain
+
+/*
+ * @GPIO_PIN_SPEEDS
+ * GPIO Pin Possible OUTPUT SPEEDS
+ */
+#define GPIO_SPEED_LOW		0
+#define GPIO_SPEED_MEDIUM	1
+#define GPIO_SPEED_FAST		2
+#define GPIO_SPEED_HIGH		3
+
+/*
+ * GPIO pin PULL UP and PULL DOWN configuration Macros
+ */
+#define GPIO_NO_PUPD 		0
+#define GPIO_PIN_PU			1
+#define GPIO_PIN_PD			2
+
+
+
 
 /*************************************************************************************
  * 								API supported by this driver
