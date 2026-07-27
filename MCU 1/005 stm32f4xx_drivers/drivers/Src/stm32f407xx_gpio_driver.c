ca31178f15877d34b15c7f0dea06bb19c70762ef
Abhimanyu P <abhimanyupazhani@gmail.com>
2026-07-27 23:07:52 +0530

    GPIO Ini5 Declaration

--- MCU 1/005 stm32f4xx_drivers/drivers/Src/stm32f407xx_gpio_driver.c
+++ MCU 1/005 stm32f4xx_drivers/drivers/Src/stm32f407xx_gpio_driver.c
@@ -7,7 +7,7 @@
 
 
 #include <stm32f407xx_gpio_driver.h>
-#include<stdio.h>
+
 
 /*
  * Peripheral Clock Setup
@@ -34,14 +34,63 @@ void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi)
 		if (pGPIOx ==  GPIOA)
 		{
 			GPIOA_PCLK_EN();
-		}elseif (pGPIOx == GPIOB)
+		}else if (pGPIOx == GPIOB)
 		{
-
+			GPIOB_PCLK_EN();
+		}else if (pGPIOx == GPIOC)
+		{
+			GPIOC_PCLK_EN();
+		}else if (pGPIOx == GPIOD)
+		{
+			GPIOD_PCLK_EN();
+		}else if (pGPIOx == GPIOE)
+		{
+			GPIOE_PCLK_EN();
+		}else if (pGPIOx == GPIOF)
+		{
+			GPIOF_PCLK_EN();
+		}else if (pGPIOx == GPIOG)
+		{
+			GPIOG_PCLK_EN();
+		}else if (pGPIOx == GPIOH)
+		{
+			GPIOH_PCLK_EN();
+		}else if (pGPIOx == GPIOI)
+		{
+			GPIOI_PCLK_EN();
 		}
 
 	}else
 	{
 		//To Do from ref manual
+		if (pGPIOx ==  GPIOA)
+				{
+					GPIOA_PCLK_DI();
+				}else if (pGPIOx == GPIOB)
+				{
+					GPIOB_PCLK_DI();
+				}else if (pGPIOx == GPIOC)
+				{
+					GPIOC_PCLK_DI();
+				}else if (pGPIOx == GPIOD)
+				{
+					GPIOD_PCLK_DI();
+				}else if (pGPIOx == GPIOE)
+				{
+					GPIOE_PCLK_DI();
+				}else if (pGPIOx == GPIOF)
+				{
+					GPIOF_PCLK_DI();
+				}else if (pGPIOx == GPIOG)
+				{
+					GPIOG_PCLK_DI();
+				}else if (pGPIOx == GPIOH)
+				{
+					GPIOH_PCLK_DI();
+				}else if (pGPIOx == GPIOI)
+				{
+					GPIOI_PCLK_DI();
+				}
 	}
 
 }
@@ -49,8 +98,58 @@ void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi)
 /*
  * Init and DeInit
  */
+
+/*********************************************************************
+ * @fn      		  - GPIO_Init
+ *
+ * @brief             - This function Initialize the GPIO
+ *
+ * @param[in]         -
+ * @param[in]         -
+ * @param[in]         -
+ *
+ * @return            -  none
+ *
+ * @Note              -  none
+
+ */
+
 void GPIO_Init(GPIO_Handle_t *pGPIOHandle)
 {
+	uint32_t temp=0;    //temp.register
+	//1, Configure the Mode of the GPIO Pin
+	if (pGPIOHandle->GPIO_PinConfig.GPIO_PinMode <= GPIO_MODE_ANALOG)
+	{
+		//Non Interrupt Mode
+		temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinMode <<(2 *pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber ));
+		pGPIOHandle->pGPIOx->MODER = temp;
+
+
+
+	}else
+	{
+		//This part will comes later (Interrupt Mode)
+	}
+	temp =0;
+	//2. Configure the speed
+	temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinSpeed << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_Pin_Number));
+	pGPIOHandle->pGPIOx->SPEEDR = temp;
+
+	temp = 0;
+
+	//3. Configure the PUPD settings
+	temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinPuPdControl  << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_Pin_Number));
+	pGPIOHandle->pGPIOx->PUPDR = temp;
+
+	temp = 0;
+	//4. Configure the OPTYPE
+	temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinOPType == l  << pGPIOHandle->GPIO_PinConfig.GPIO_Pin_Number);
+	pGPIOHandle->pGPIOx->OTYPER = temp;
+
+
+
+	//5. Configure the ALT Functionality
+
 
 }
 void GPIO_DeInit(GPIO_RegDef_t *pGPIOx)
